ceda6b1a0e8184bc18537eaae8b1fd199d4f767f
MercurinH <Mercurinh@gmail.com>
2023-03-13 17:53:47 +0100

    Création2

---
+++ WeatherGuard/src/main/java/com/example/weatherguard/WeatherGuardApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherGuardApplication.class, args);
+    }
+
+}
---
+++ WeatherGuard/src/test/java/com/example/weatherguard/WeatherGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
