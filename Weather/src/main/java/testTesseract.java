1a707804be72683e8f6867b8217df53de33b11de
MercurinH <Mercurinh@gmail.com>
2023-03-21 15:51:59 +0100

    last push

--- Weather/src/main/java/testTesseract.java
+++ Weather/src/main/java/testTesseract.java
@@ -6,10 +6,11 @@ import java.util.regex.Pattern;
 
 public class testTesseract {
     public static void main(String[] args) throws TesseractException, IOException {
+        // Configuration du processus Tesseract
         ProcessBuilder processBuilder = new ProcessBuilder();
         processBuilder.command("tesseract", "/Users/hugo/Desktop/Cours/MasterMIAGEM2/S2/hackathon/Weather/src/main/resources/images/re-u-parking.jpeg", "output");
         try {
-
+            // Lancement de Tesseract pour effectuer la reconnaissance optique de caractères
             Process process = processBuilder.start();
             StringBuilder output = new StringBuilder();
             BufferedReader reader = new BufferedReader(
@@ -18,12 +19,13 @@ public class testTesseract {
             while ((line = reader.readLine()) != null) {
                 output.append(line + "\n");
             }
+            // Vérification du résultat de Tesseract
             int exitVal = process.waitFor();
             if (exitVal == 0) {
-                System.out.println("Success!");
+                System.out.println("Reconnaissance de caractères réussie !");
                 System.out.println(output);
             } else {
-                System.out.println("Failed!");
+                System.out.println("Erreur lors de la reconnaissance de caractères !");
                 System.exit(1);
             }
         } catch (IOException e) {
@@ -32,10 +34,12 @@ public class testTesseract {
             e.printStackTrace();
         }
 
+        // Récupération du texte reconnu à partir du fichier de sortie de Tesseract
         FileInputStream fis = new FileInputStream("/Users/hugo/Desktop/Cours/MasterMIAGEM2/S2/hackathon/output.txt");
         String data = IOUtils.toString(fis, "UTF-8");
         System.out.println(data);
-        // chercher la ligne contenant "ORLEANS" à l'aide d'une expression régulière
+
+        // Recherche de la localisation
         Pattern pattern = Pattern.compile("(?i).*\\bORLEANS\\b.*");
         Matcher matcher = pattern.matcher(data);
         if (matcher.find()) {
@@ -44,7 +48,8 @@ public class testTesseract {
         } else {
             System.out.println("Localisation non trouvée.");
         }
-        // chercher la date d'entrée
+
+        // Recherche de la date d'entrée
         pattern = Pattern.compile("ENTR\\.\\s*(\\d{2}\\.\\d{2}\\.\\d{2})");
         matcher = pattern.matcher(data);
         if (matcher.find()) {
@@ -53,7 +58,8 @@ public class testTesseract {
         } else {
             System.out.println("Date d'entrée non trouvée.");
         }
-        // chercher la date de sortie
+
+        // Recherche de la date de sortie
         pattern = Pattern.compile("PAYE\\.\\s*(\\d{2}\\.\\d{2}\\.\\d{2})");
         matcher = pattern.matcher(data);
         if (matcher.find()) {
@@ -62,7 +68,8 @@ public class testTesseract {
         } else {
             System.out.println("Date de sortie non trouvée.");
         }
-        // Utilisation d'une expression régulière pour extraire la valeur associée à "PRIX TTC"
+
+        // Recherche du prix TTC
         String prixTTCRegex = "PRIX TTC:\\s*(\\d+\\.\\d{2})\\s*EU";
         pattern = Pattern.compile(prixTTCRegex);
         matcher = pattern.matcher(data);
@@ -72,7 +79,5 @@ public class testTesseract {
         } else {
             System.out.println("Le prix TTC n'a pas été trouvé.");
         }
-
-
     }
 }
